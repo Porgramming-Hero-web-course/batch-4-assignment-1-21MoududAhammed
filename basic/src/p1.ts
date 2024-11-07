3837c016d42d7a304142c0865837d5ba706ca993
21MoududAhammed <21moudud@gmail.com>
2024-11-07 21:02:58 +0600

    The soluton of problem one has done

--- basic/src/p1.ts
+++ basic/src/p1.ts
@@ -1,2 +1,12 @@
-const num1 : number = 100;
-console.log(num1)
+// Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
+
+function sumArray(arrayOfNum: number[]): number {
+  let sum: number = 0;
+  arrayOfNum.forEach((item: number): number => (sum += item));
+
+  return sum;
+}
+
+const myNumbers = [10, 20, 30];
+const result = sumArray(myNumbers);
+console.log(result);
